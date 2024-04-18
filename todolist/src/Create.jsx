9d6d7423f35daa205fe5644686909358041048dc
gaoyan <magaoyanxj@gmail.com>
2024-04-18 10:05:04 +0800

    create Create

---
+++ todolist/src/Create.jsx
@@ -0,0 +1,11 @@
+
+function Create() {
+  return (
+    <div>
+      <input type="text" name="" id="" />
+      <button type="button">Add</button>
+    </div>
+  )
+}
+
+export default Create
--- todolist/src/Home.jsx
+++ todolist/src/Home.jsx
@@ -1,8 +1,10 @@
+import Create from "./Create"
 
 function Home() {
   return (
     <div>
       <h2>Todo List</h2>
+      <Create />
     </div>
   )
 }
